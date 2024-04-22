13cb63271b11f6d27fbb027e63dc1101cb3c9b1a
Abenaya19 <165239735+Abenaya19@users.noreply.github.com>
2024-04-22 12:48:04 +0530

    Delete WebFrameworks Classwork/AbeDay-5/class_exercise directory

--- WebFrameworks Classwork/AbeDay-5/class_exercise/abeday5cw3/src/main/java/com/example/day5cw3/aberepository/AbeLanguageRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.abeday5cw3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.day5cw3.model.AbeLanguage;
-
-public interface AbeLanguageRepo extends JpaRepository<AbeLanguage,Integer>{
-    
-}
